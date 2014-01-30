80b02f136ad5ff44197a20874de1bd5d494334e2
Seamus Tuohy <s2e@opentechinstitute.org>
2014-01-30 17:21:00 -0500

    added a json config reader for gathering global, module, and user commotion-client config files. NOT commotiond profiles.

--- commotion_client/utils/config.py
+++ commotion_client/utils/config.py
@@ -7,79 +7,108 @@ config
 
 The configuration manager.
 """
-
-import configparser  #sorry Jordan...
+import sys
 import os
+import json
+import logging
+
+from PyQt4 import QtCore
+
+from utils import fsUtils
 
 #set function logger
 log = logging.getLogger("commotion_client."+__name__) #TODO commotion_client is still being called directly from one level up so it must be hard coded as a sub-logger if called from the command line.
 
-
-def splitConfig(config):
+def findConfigs(configType, name=None):
     """
-    Iterator that returns all configuration files (sections) nested in a specific config.
+    Function used to obtain the path to a config file.
+
+    @param configType The type of configuration file sought. (global, user, module)
+    @param name optional The name of the configuration file if known
+
+    @return list of tuples containing the path and name of found config files or False if a config matching the description cannot be found.
     """
-    sections = config.section()
-    for section in sections:
-        yield config[section]
-
-
-def findConfig(config_type):
-    config = [
-
-    #for 
-    #if getConfig(name):
-
-        
-def getConfig(name):
-    """
-    Function to get a config file.
-
-    @param name the name of the configuration file to retreive
-    """
-    #load config file
-    try:
-        checkFile(name) #TODO
-        path = configPath(name)
-        config = loadConfig(path)
-    except:
-        log.error(QtCore.QCoreApplication.translate("logs", "Config file {0} cannot be loaded.".format(name)))
+    configFiles = getConfigPaths(configType)
+    configs = list(getConfig(configFiles))
+    if configs == []:
+        log.error(QtCore.QCoreApplication.translate("logs", "No Configs of the chosed type found"))
+        return False
+    elif name != None:
+        for conf in configs:
+            if conf["name"] and conf["name"] == name:
+                return conf
+        log.error(QtCore.QCoreApplication.translate("logs", "No config of the chosed type named {0} found".format(name)))
         return False
     else:
-        return config
+        return configs
+
+def getConfigPaths(configType):
+
+    configLocations = {"global":"data/global/", "user":"data/user/", "module":"data/modules/"}
+    configFiles = []
     
-
-def checkFile(name):
-    
-        
-def addSectionHeader(properties_file, header_name):
-   """
-   add a config file does not have a section header. This adds it.
-   
-   configparser.ConfigParser requires at least one section header in a properties file.
-   Our properties file doesn't have one, so add a header to it on the fly.
-   https://stackoverflow.com/questions/2819696/parsing-properties-file-in-python/2819788#2819788
-   """
-   yield '[{}]\n'.format(header_name)
-   for line in properties_file:
-     yield line
-
-
-def loadConfig(path):
-    """
-    Load a single config file.
-
-    @param path the path to the config file
-    @return parsed config file
-    """
-    config_file = open(path, encoding="utf_8")
-    config = configparser.ConfigParser()
-    default_section_header = "CONFIG"
     try:
-        config.read_file(config_file)
-    except MissingSectionHeaderError:
-        config.read_file(addSectionHeader(config_file, default_section_header))
-    except:
-        log.error(QtCore.QCoreApplication.translate("logs", "Config file {0} is not correctly formatted.".format(name)))
+        path = configLocations[configType]
+    except KeyError:
+        log.error(QtCore.QCoreApplication.translate("logs", "Cannot search for config type {0} as it is an unsupported type.".format(configType)))
+        return False
     else:
-        return config
+        for root, dirs, files in fsUtils.walklevel(path):
+            for file_name in files:
+                if file_name.endswith(".conf"):
+                    configFiles.append(os.path.join(root, file_name))
+    if configFiles != []:
+        return configFiles
+    else:
+        return False
+
+
+def getConfig(paths):
+    """
+    Generator to retreive config files for the paths passed to it
+
+    @param a list of paths of the configuration file to retreive
+    @return config file as a dictionary
+    """
+    #load config file
+    for path in paths:
+        try:
+            if fsUtils.is_file(path):
+                config = loadConfig(path)
+                if config:
+                    yield config
+            else:
+                log.error(QtCore.QCoreApplication.translate("logs", "Config file {0} does not exist and therefore cannot be loaded.".format(path)))
+        except:
+            log.error(QtCore.QCoreApplication.translate("logs", "Config file {0} cannot be loaded.".format(path)))
+        
+
+def loadConfig(config):
+    """
+    This function loads a json formatted config file and returns it.
+
+    @param fileName the name of the config file
+    @param path the path to the configuration file in question
+    @return a dictionary containing the config files values
+    """
+    #Open the file
+    try:
+        f = open(config, mode='r', encoding="utf-8", errors="strict")
+    except ValueError:
+        log.error(QtCore.QCoreApplication.translate("logs", "Config files must be in utf-8 format to avoid data loss. The config file {0} is improperly formatted ".format(config)))
+        return False
+    except:
+        log.error(QtCore.QCoreApplication.translate("logs", "An unknown error has occured in opening config file {0}. Please check that this file exists and is not corrupted.".format(config)))
+        return False
+    else:
+        tmpMsg = f.read()
+    #Parse the JSON
+    try:
+        data = json.loads(tmpMsg)
+        log.debug(QtCore.QCoreApplication.translate("logs", "Successfully loaded {0}".format(config)))
+        return data
+    except ValueError:
+        log.error(QtCore.QCoreApplication.translate("logs", "Failed to load {0} due to a non-json or otherwise invalid file type".format(config)))
+        return False
+    except:
+        log.error(QtCore.QCoreApplication.translate("logs", "Failed to load {0} due to an unknown error.".format(config)))
